e27dcd1df7018ea7faaca4d57d481e0608224fc6
Steven Lee <1827855859@qq.com>
2020-09-07 04:29:07 +1000

    0907 early Steven

---
+++ app/src/main/java/com/example/skyscraper/Doctor1Activity.java
@@ -0,0 +1,13 @@
+package com.example.skyscraper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Doctor1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctor_1);
+    }
+}
--- app/src/main/java/com/example/skyscraper/MainPageActivity.java
+++ app/src/main/java/com/example/skyscraper/MainPageActivity.java
@@ -1,7 +1,13 @@
 package com.example.skyscraper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.example.skyscraper.ui.login.LoginActivity;
 
 public class MainPageActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        final ImageButton loginButton = findViewById(R.id.doctor1Button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainPageActivity.this, Doctor1Activity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
